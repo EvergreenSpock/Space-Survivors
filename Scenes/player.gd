866518475fb74833a0a12912c6948015c4a1bebb
EnvyingGolem47 <rross2693@gmail.com>
2025-05-17 11:15:21 -0700

    Added boost feature w/ particles
    It's pretty rudimentary but it looks pretty good imo

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,11 +2,18 @@ extends CharacterBody3D
 
 # How fast the player moves in meters per second.
 @export var speed = 14
+
 # The downward acceleration when in the air, in meters per second squared.
 #@export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
 
+var leftBoostTrail: GPUParticles3D
+var rightBoostTrail: GPUParticles3D
+
+func _ready():
+	leftBoostTrail = get_node("Pivot/MicroRecon/LeftEngineBoostTrail")
+	rightBoostTrail = get_node("Pivot/MicroRecon/RightEngineBoostTrail")
 
 func _physics_process(_delta):
 	var direction = Vector3.ZERO
@@ -19,6 +26,20 @@ func _physics_process(_delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+
+	# If boosting, increase the speed & turn on particles
+	if Input.is_action_pressed("boost"):
+		speed = 28
+		
+		leftBoostTrail.emitting = true
+		rightBoostTrail.emitting = true
+		
+	else: # could probably do this better and not have run every update frame
+		speed = 14
+		
+		leftBoostTrail.emitting = false
+		rightBoostTrail.emitting = false
+
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
