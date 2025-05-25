bc0780e295fe9d7e4caabd83f824d0e2808b3caa
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2025-05-24 20:54:04 -0700

    random commit

--- Scripts/bullet_pew_pew.gd
+++ Scripts/bullet_pew_pew.gd
@@ -11,11 +11,12 @@ const SPEED = 80.0;
 func _process(delta):
 	global_position += -global_transform.basis.z * SPEED * delta
 	if ray.is_colliding():
-		#mesh.visble = false
+		mesh.visible = false
 		particles.emitting = true
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
 
 
 func _on_timer_timeout() -> void:
+	particles.emitting = true
 	queue_free()
