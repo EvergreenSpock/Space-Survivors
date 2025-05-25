ae4a71414aeba2068df3e7ae2c81d173cc8ebbd5
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2025-05-25 16:21:59 -0700

    Sniper bot works

---
+++ Scenes/heavy_bullet.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+
+const SPEED = 100.0;
+
+@export var damage: int = 50 #Heavy damage bullet
+
+@onready var mesh = $BulletMesh
+@onready var ray = $RayCast3D
+@onready var particles = $GPUParticles3D
+
+
+signal hit_something(target: Node, damage: int)
+
+func _process(delta):
+	var collider = ray.get_collider()
+	global_position += -global_transform.basis.z * SPEED * delta
+	if collider and collider.has_method("apply_damage"):
+		emit_signal("hit_something", collider, damage)
+		collider.apply_damage(damage)
+		particles.emitting = true
+		mesh.visible = false
+		await get_tree().create_timer(0.5).timeout
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	particles.emitting = true
+	queue_free()
